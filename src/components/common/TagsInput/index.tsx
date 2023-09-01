517d6796421faad61868a6fa03685442bdaa9c18
elbotanist <ahmedelgaidi260@gmail.com>
2023-09-01 04:54:10 +0300

    Added hte functionality of remvoing a teag from form

--- src/components/common/TagsInput/index.tsx
+++ src/components/common/TagsInput/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import styles from "./index.module.css";
 import { ITagsInput } from "./index.types";
+import { AiFillCloseCircle } from "react-icons/ai";
 
 export default function TagsInput({ label = "Tags*", prefix = "#" }: ITagsInput) {
 	const [tags, setTags] = useState<string[]>([]);
@@ -15,6 +16,11 @@ export default function TagsInput({ label = "Tags*", prefix = "#" }: ITagsInput)
 		}
 	};
 
+	const removeTagHandler = (tagIndex: number) => {
+		const updatedTags = tags.filter((_, index) => index !== tagIndex);
+		setTags(updatedTags);
+	};
+
 	return (
 		<div className={styles.tags_input_wrapper}>
 			<label className={styles.tags_input_label} htmlFor="tagsInput">
@@ -24,7 +30,10 @@ export default function TagsInput({ label = "Tags*", prefix = "#" }: ITagsInput)
 				<ul className={styles.tags_input_items}>
 					{tags.map((el, i) => (
 						<li className={styles.tags_input_item} key={i}>
-							{prefix ? prefix + el : el}
+							<span className={styles.tags_input_item_text}>{prefix ? prefix + el : el}</span>
+							<span className={styles.tags_input_item_close_icon} onClick={() => removeTagHandler(i)}>
+								<AiFillCloseCircle />
+							</span>
 						</li>
 					))}
 				</ul>
