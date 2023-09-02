7830e91d9e21f970d17e9cf96b69233b7be7801c
elbotanist <ahmedelgaidi260@gmail.com>
2023-09-02 16:04:54 +0300

    Some code refactoring

--- src/components/Dashboard/index.data.ts
+++ src/components/Dashboard/index.data.ts
@@ -1,6 +1,6 @@
 import { IAnalyticsDataItem } from "../Common/Home/index.types";
 import { IListItem } from "../Common/List/index.types";
-import { ISectionData } from "./index.types";
+import { ISectionData, TCurrentPath } from "./index.types";
 
 export const sections: Record<string, ISectionData> = {
 	posts: {
@@ -57,9 +57,29 @@ export const listItems: IListItem[] = [
 	},
 ];
 
-export const homeAnalyticsData: IAnalyticsDataItem[] = [
+const postsAnalyticsData: IAnalyticsDataItem[] = [
 	{ title: "Total Posts Views:", value: 1000, unit: "Views" },
 	{ title: "Total Posts:", value: 1000, unit: "Posts" },
 	{ title: "Total Published Posts:", value: 1000, unit: "Posts" },
 	{ title: "Total Unpublished Posts:", value: 1000, unit: "Posts" },
 ];
+
+const walkthroughsAnalyticsData: IAnalyticsDataItem[] = [
+	{ title: "Total Walkthroughs Views:", value: 800, unit: "Views" },
+	{ title: "Total Walkthroughs:", value: 600, unit: "Walkthroughs" },
+	{ title: "Total Published Walkthroughs:", value: 400, unit: "Walkthroughs" },
+	{ title: "Total Unpublished Walkthroughs:", value: 200, unit: "Walkthroughs" },
+];
+
+const seriesAnalyticsData: IAnalyticsDataItem[] = [
+	{ title: "Total Series Views:", value: 1200, unit: "Views" },
+	{ title: "Total Series:", value: 900, unit: "Series" },
+	{ title: "Total Published Series:", value: 700, unit: "Series" },
+	{ title: "Total Unpublished Series:", value: 200, unit: "Series" },
+];
+
+export const analyticsDataByPath: Record<TCurrentPath, IAnalyticsDataItem[]> = {
+	"/dashboard/posts/home": postsAnalyticsData,
+	"/dashboard/walkthroughs/home": walkthroughsAnalyticsData,
+	"/dashboard/series/home": seriesAnalyticsData,
+};
