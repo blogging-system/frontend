a2b047dbdaf079bcbe12e248b3de8f085696108a
9abour <9abour@gmail.com>
2023-11-30 18:17:23 +0200

    Fix getUser.

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,7 @@ const middleware = async (request: NextRequest) => {
 	// Redirects
 
 	// if not authenticated redirect to login page
-	const user = "";
+	const user = await getUser();
 
 	if (!request.nextUrl.pathname.includes("auth") && !user) {
 		return NextResponse.redirect(new URL("/auth/login", request.url));
