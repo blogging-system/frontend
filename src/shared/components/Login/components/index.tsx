2cd995b4f24888a7576725097fc76c31c008f64f
elbotanist <ahmedelgaidi260@gmail.com>
2023-12-09 00:44:26 +0200

    Fixed some local conflicts

--- src/shared/components/Login/components/index.tsx
+++ src/shared/components/Login/components/index.tsx
@@ -1,78 +1,66 @@
-"use client";
-
-<<<<<<< HEAD:src/shared/components/Login/components/index.tsx
-import { useRouter } from "next/navigation";
-import styles from "../styles/index.module.css";
-import FormItem from "@/shared/components/Common/Form/components/FormItem";
-=======
-import FormItem from "@/components/Common/Form/components/FormItem";
-import { login } from "../services/login.service";
-import styles from "../styles/index.module.css";
-import useInput from "@/hooks/inputs/useInput";
-import { FormEvent, useEffect, useState } from "react";
-import { handleApiRequest } from "@/helpers/services/handleApiRequest.helper";
->>>>>>> fdf80734e79afe2ffffb2014e9d6d3b975137d1c:src/components/Login/components/index.tsx
+'use client'
+import styles from '../styles/index.module.css'
+import FormItem from '@/shared/components/Common/Form/components/FormItem'
+import useInput from '@/shared/hooks/inputs/useInput'
+import { FormEvent, useEffect, useState } from 'react'
+import { login } from '@/components/Login/services/login.service'
 
 export default function LogIn() {
-	const email = useInput("");
-	const password = useInput("");
-	const [isLoading, setIsLoading] = useState(false);
-	const [errorMsg, setErrorMsg] = useState(null);
+  const email = useInput('')
+  const password = useInput('')
+  const [isLoading, setIsLoading] = useState(false)
+  const [errorMsg, setErrorMsg] = useState(null)
 
-	const handleLoginSubmit = async (e: FormEvent) => {
-		e.preventDefault();
-		setIsLoading(true);
+  const handleLoginSubmit = async (e: FormEvent) => {
+    e.preventDefault()
+    setIsLoading(true)
 
-		const { error } = await login({
-			email: email.value,
-			password: password.value,
-		});
+    const { error } = await login({
+      email: email.value,
+      password: password.value,
+    })
 
-		setIsLoading(false);
+    setIsLoading(false)
 
-		if (error) {
-			setErrorMsg(error);
-		}
-	};
+    if (error) {
+      setErrorMsg(error)
+    }
+  }
 
-	useEffect(() => {
-		setErrorMsg(prev => prev && null);
-	}, [email.value, password.value]);
+  useEffect(() => {
+    setErrorMsg((prev) => prev && null)
+  }, [email.value, password.value])
 
-	return (
-		<div className={styles.login_wrapper}>
-			<form
-				action=""
-				className={styles.login_form}
-				onSubmit={handleLoginSubmit}
-			>
-				<h2 className={styles.login_title}>Login Page</h2>
+  return (
+    <div className={styles.login_wrapper}>
+      <form action="" className={styles.login_form} onSubmit={handleLoginSubmit}>
+        <h2 className={styles.login_title}>Login Page</h2>
 
-				<FormItem
-					label="Email"
-					placeholder="Please enter your email"
-					type="text"
-					name="email"
-					required={true}
-					{...email}
-				/>
-				<FormItem
-					label="Password"
-					placeholder="Please enter your password"
-					type="password"
-					name="password"
-					required={true}
-					{...password}
-				/>
+        <FormItem
+          label="Email"
+          placeholder="Please enter your email"
+          type="text"
+          name="email"
+          required={true}
+          {...email}
+        />
+        <FormItem
+          label="Password"
+          placeholder="Please enter your password"
+          type="password"
+          name="password"
+          required={true}
+          {...password}
+        />
 
-				{errorMsg && <p className={styles.errorMsg}>{[...errorMsg]}</p>}
+        {errorMsg && <p className={styles.errorMsg}>{[...errorMsg]}</p>}
 
-				<div className={styles.login_button_wrapper}>
-					<button className={styles.login_button} type="submit">
-						{isLoading ? "Loading..." : "Log In"}
-					</button>
-				</div>
-			</form>
-		</div>
-	);
+        <div className={styles.login_button_wrapper}>
+          <button className={styles.login_button} type="submit">
+            {isLoading ? 'Loading...' : 'Log In'}
+          </button>
+        </div>
+      </form>
+    </div>
+  )
 }
