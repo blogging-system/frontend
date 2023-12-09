51db3043846c5ba17487a14f02dadbb2d92f5432
elbotanist <ahmedelgaidi260@gmail.com>
2023-12-09 12:39:44 +0200

    saving local changes

--- src/components/Dashboard/components/tags/TagsList.tsx
+++ src/components/Dashboard/components/tags/TagsList.tsx
@@ -1,38 +1,36 @@
-import React, { useEffect } from "react";
-import styles from "../../styles/tagsList.module.css";
-import { useParams } from "next/navigation";
-import TagListItem from "./TagListItem";
-import { useAppDispatch, useAppSelector } from "@/rtk/hooks";
-import { fetchTagsList } from "@/rtk/slices/tagsListSlice";
+import React, { useEffect } from 'react'
+import styles from '../../styles/tagsList.module.css'
+import { useParams } from 'next/navigation'
+import TagListItem from './TagListItem'
+import { useAppDispatch, useAppSelector } from '@/rtk/hooks'
+import { fetchTagsList } from '@/rtk/slices/tagsListSlice'
 
 const TagsList = () => {
-	const { slug } = useParams();
+  const { slug } = useParams()
 
-	const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch()
 
-	const { tagsList, error, isLoading } = useAppSelector(
-		state => state.tagsList
-	);
+  const { tagsList, error, isLoading } = useAppSelector((state) => state.tagsList)
 
-	useEffect(() => {
-		dispatch(fetchTagsList(slug[slug.length - 1]));
-	}, []);
+  useEffect(() => {
+    dispatch(fetchTagsList(slug[slug.length - 1]))
+  }, [])
 
-	return (
-		<div className={styles.tags_list_wrapper}>
-			{tagsList ? (
-				<ul className={styles.list_items}>
-					{tagsList.map((tag, i) => (
-						<TagListItem key={i} tag={tag} />
-					))}
-				</ul>
-			) : !error || isLoading ? (
-				<h1>Loading...</h1>
-			) : (
-				error
-			)}
-		</div>
-	);
-};
+  return (
+    <div className={styles.tags_list_wrapper}>
+      {tagsList ? (
+        <ul className={styles.list_items}>
+          {tagsList.map((tag, i) => (
+            <TagListItem key={i} tag={tag} />
+          ))}
+        </ul>
+      ) : !error || isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        error
+      )}
+    </div>
+  )
+}
 
-export default TagsList;
+export default TagsList
